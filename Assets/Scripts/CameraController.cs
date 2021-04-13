2fb6f4ea2835ed415c285ca41913073a6fa4ac97
jnovilla <jnovilla6395@sdsu.edu>
2021-04-13 14:27:40 -0700

    added camera controller to follow the player movement.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerController Player;  //player data
+
+    private Vector3 OldPlayerPosition;  
+    private float distanceCameraMove;  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = FindObjectOfType<PlayerController>();
+        OldPlayerPosition = Player.transform.position;  //player position at start
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceCameraMove = Player.transform.position.x - OldPlayerPosition.x; //camera moves this amount
+
+        transform.position = new Vector3(transform.position.x + distanceCameraMove, transform.position.y, transform.position.z);  //camera moves only on x axis changes of the player
+
+        OldPlayerPosition = Player.transform.position; //updated position of player
+    }
+}
