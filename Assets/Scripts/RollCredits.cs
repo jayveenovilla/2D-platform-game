cbd510ad0538b63693d22b055f0bfca7b4cab4bb
jnovilla <jnovilla6395@sdsu.edu>
2021-04-15 20:19:37 -0700

    added names and positions to credits scene. rolling text in credits scene to be completed at a later date.

---
+++ Assets/Scripts/RollCredits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollCredits : MonoBehaviour
+{
+    public Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myAnimator.Play("Credits", 0, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnimator.Play("Credits",0,0);
+    }
+}
