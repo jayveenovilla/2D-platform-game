be93f527b40354b83cd965eef481e67eb6b4cf2e
jnovilla <jnovilla6395@sdsu.edu>
2021-04-16 23:13:29 -0700

    added scrolling background and script. background moves along x axis only. possible future ability for player to select background image via bonus selection screen.

---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    //used for the scrolling background
+    Material myMaterial;
+
+    Vector2 offsetBackground;
+
+    public float velocityX; //speed background moves in x direction
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetBackground = new Vector2(velocityX, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offsetBackground * Time.deltaTime;      //offset is multiplied by delta time
+    }
+
+    private void Awake()
+    {
+        myMaterial = GetComponent<Renderer>().material;     //get component Material
+    }
+}
